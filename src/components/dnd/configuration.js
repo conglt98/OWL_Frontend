ae520b15a055e8d5f6f02ba13edc60792be46dac
1612842 <1612842@student.hcmus.edu.vn>
2020-06-29 02:03:14 +0700

    config

--- src/components/dnd/configuration.js
+++ src/components/dnd/configuration.js
@@ -1,14 +1,176 @@
-import React from 'react'
-import Dnd from './index'
-import {Card, CardBody} from 'reactstrap'
-export default class Demo extends React.Component{
-    render(){
-        return(
-        <Card>
-            <CardBody>
-                <Dnd></Dnd>
-            </CardBody>
-        </Card>
-        )
+import React, { useState } from 'react';
+import { Table, Input, InputNumber, Popconfirm, Form } from 'antd';
+
+const originData = [
+    {
+        key: '1',
+        host: 'AutoTraining',
+        url: 'http://',
+    },
+    {
+        key: '2',
+        host: 'Crawler&Detect',
+        url: 'http://',
+    },
+    {
+        key: '3',
+        host: 'Result',
+        url: 'http://',
     }
-}
+];
+
+const EditableCell = ({
+  editing,
+  dataIndex,
+  title,
+  inputType,
+  record,
+  index,
+  children,
+  ...restProps
+}) => {
+  const inputNode = inputType === 'number' ? <InputNumber /> : <Input />;
+  return (
+    <td {...restProps}>
+      {editing ? (
+        <Form.Item
+          name={dataIndex}
+          style={{
+            margin: 0,
+          }}
+          rules={[
+            {
+              required: true,
+              message: `Please Input ${title}!`,
+            },
+          ]}
+        >
+          {inputNode}
+        </Form.Item>
+      ) : (
+        children
+      )}
+    </td>
+  );
+};
+
+const EditableTable = () => {
+  const [form] = Form.useForm();
+  const [data, setData] = useState(localStorage.getItem('url')?JSON.parse(localStorage.getItem('url')):originData);
+  const [editingKey, setEditingKey] = useState('');
+
+  const isEditing = record => record.key === editingKey;
+
+  const edit = record => {
+    form.setFieldsValue({
+      host: '',
+      url: '',
+      ...record,
+    });
+    setEditingKey(record.key);
+  };
+
+  const cancel = () => {
+    setEditingKey('');
+  };
+
+  const save = async key => {
+    try {
+      const row = await form.validateFields();
+      const newData = [...data];
+      const index = newData.findIndex(item => key === item.key);
+
+      if (index > -1) {
+        const item = newData[index];
+        newData.splice(index, 1, { ...item, ...row });
+        setData(newData);
+        setEditingKey('');
+      } else {
+        newData.push(row);
+        setData(newData);
+        setEditingKey('');
+      }
+      localStorage.setItem("url",JSON.stringify(newData))
+    } catch (errInfo) {
+      console.log('Validate Failed:', errInfo);
+    }
+  };
+
+  const columns = [
+    {
+      title: 'host',
+      dataIndex: 'host',
+      width: '25%',
+      editable: true,
+    },
+    {
+      title: 'url',
+      dataIndex: 'url',
+      width: '40%',
+      editable: true,
+    },
+    {
+      title: 'operation',
+      dataIndex: 'operation',
+      render: (_, record) => {
+        const editable = isEditing(record);
+        return editable ? (
+          <span>
+            <a
+              href="javascript:;"
+              onClick={() => save(record.key)}
+              style={{
+                marginRight: 8,
+              }}
+            >
+              Save
+            </a>
+            <Popconfirm title="Sure to cancel?" onConfirm={cancel}>
+              <a>Cancel</a>
+            </Popconfirm>
+          </span>
+        ) : (
+          <a disabled={editingKey !== ''} onClick={() => edit(record)}>
+            Edit
+          </a>
+        );
+      },
+    },
+  ];
+  const mergedColumns = columns.map(col => {
+    if (!col.editable) {
+      return col;
+    }
+
+    return {
+      ...col,
+      onCell: record => ({
+        record,
+        inputType: col.dataIndex === 'age' ? 'number' : 'text',
+        dataIndex: col.dataIndex,
+        title: col.title,
+        editing: isEditing(record),
+      }),
+    };
+  });
+  return (
+    <Form form={form} component={false}>
+      <Table
+        components={{
+          body: {
+            cell: EditableCell,
+          },
+        }}
+        bordered
+        dataSource={data}
+        columns={mergedColumns}
+        rowClassName="editable-row"
+        pagination={{
+          onChange: cancel,
+        }}
+      />
+    </Form>
+  );
+};
+
+export default EditableTable
